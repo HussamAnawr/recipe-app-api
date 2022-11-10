f48cc4b956c4ceee8744efc35c6ee2b115e94f5b
Hussam <o.hussam@gmail.com>
2022-11-10 13:32:24 +0300

    fixint init for management command

--- app/core/management/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Django management command to wait for database avaliablity
-"""
-
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        pass
