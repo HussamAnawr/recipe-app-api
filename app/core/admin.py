83e29350043f69f014b6aac68861ea1ae0bcda6f
Hussam <o.hussam@gmail.com>
2022-11-14 12:08:06 +0300

    Configuring Admin Site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin
 
+from django.contrib.auth.admin  import UserAdmin as BaseUserAdmin
+from django.contrib import admin
+from . import models
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (_('Basic Info'), {
+            "fields": (
+                'name', 
+                'email'
+            ),
+        }),
+        (_('Permission'), {
+            'fields':(
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None , {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+    
+
+admin.site.register(models.User, UserAdmin)
